1dea4d1e26c11aa23243b85cf16ab26e19855c76
eddie <eddie@5xruby.tw>
2021-09-11 15:35:21 +0800

    basic layout

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,7 +3,8 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = []
 
-  connect() {
-    console.log("hi")
+  close(e) {
+    e.preventDefault()
+    this.element.remove()
   }
 }
