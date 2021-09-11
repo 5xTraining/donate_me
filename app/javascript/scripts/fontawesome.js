ebb48df69636a0c686fbfd7862009221b296931b
eddie <eddie@5xruby.tw>
2021-09-12 01:26:14 +0800

    update CSS

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -1,7 +1,9 @@
 import { library, dom, config } from "@fortawesome/fontawesome-svg-core"
 import { faCommentDollar } from "@fortawesome/free-solid-svg-icons/faCommentDollar"
 import { faMoneyCheckAlt } from "@fortawesome/free-solid-svg-icons/faMoneyCheckAlt"
+import { faTimes } from "@fortawesome/free-solid-svg-icons/faTimes"
 
 config.mutateApproach = "sync"
-library.add(faCommentDollar, faMoneyCheckAlt)
+library.add(faCommentDollar, faMoneyCheckAlt, faTimes)
+
 dom.watch()
