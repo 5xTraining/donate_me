d8013b08b5a4037e4c73ddef127571abcf6787f3
eddie <eddie@5xruby.tw>
2021-09-11 21:17:51 +0800

    sync loading for fontawesome icons

--- app/javascript/scripts/fontawesome.js
+++ app/javascript/scripts/fontawesome.js
@@ -1,6 +1,7 @@
-import { library, dom } from "@fortawesome/fontawesome-svg-core"
+import { library, dom, config } from "@fortawesome/fontawesome-svg-core"
 import { faCommentDollar } from "@fortawesome/free-solid-svg-icons/faCommentDollar"
 import { faMoneyCheckAlt } from "@fortawesome/free-solid-svg-icons/faMoneyCheckAlt"
 
-library.add(faMoneyCheckAlt, faCommentDollar)
+config.mutateApproach = "sync"
+library.add(faCommentDollar, faMoneyCheckAlt)
 dom.watch()
